42e5676e2944cdd3f5a46ae515539a7bb6d7b160
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-11-12 00:19:48 +0100

    Update wsuploadwtxtjson.py

--- Larramendi/wikisource/wsuploadwtxtjson.py
+++ Larramendi/wikisource/wsuploadwtxtjson.py
@@ -9,12 +9,14 @@ with open('D:/Lab_LAR/anchoreddict.json', 'r', encoding="utf-8") as json_file:
 
 for page in anchoreddict:
     pagetitle = page.replace(' ', '_')
-    print ('Now processing page '+pagetitle+'...')
-    wtxtfile = 'D:/Lab_LAR/WTXT_anchored/'+pagetitle.replace('.', '_').replace('/', '_')
-    with open(wtxtfile, 'w', encoding='utf-8') as singlefile:
-        singlefile.write(anchoreddict[page])
-    botcommandline = "python D:/Lab_LAR/pywikibot/pwb.py pagefromfile.py -textonly -notitle -force -file:"+wtxtfile+" -title:"+pagetitle
-    args = shlex.split(botcommandline)
-    print(args)
-    p = subprocess.Popen(args)
-    time.sleep(1)
+    pagenum = int(re.search(r'pdf/([0-9]+)', pagetitle).group(1))
+    if pagenum > 1130: # page number range to upload
+        print ('Now processing page '+str(pagenum)+'...')
+        wtxtfile = 'D:/Lab_LAR/WTXT_anchored/'+pagetitle.replace('.', '_').replace('/', '_').replace(':','_')
+        with open(wtxtfile, 'w', encoding='utf-8') as singlefile:
+            singlefile.write(anchoreddict[page])
+        botcommandline = "python D:/Lab_LAR/pywikibot/pwb.py pagefromfile.py -textonly -notitle -force -file:"+wtxtfile+" -title:"+pagetitle
+        args = shlex.split(botcommandline)
+        print(args)
+        p = subprocess.Popen(args)
+        time.sleep(5) # seems to be important
