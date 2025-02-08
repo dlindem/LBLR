58b79ee2fc7aa41d9edc47a40880e9d472c20c59
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-02-08 17:03:50 +0100

    Update wikitext_aingurak_jarri.py

--- Lexikoiak/wikitext_aingurak_jarri.py
+++ Lexikoiak/wikitext_aingurak_jarri.py
@@ -1,24 +1,23 @@
+import re
 
-
-with open('data/etxepare.wikitext') as file:
+with open('data/SanAgustin.wikitext') as file:
     wikitext = file.read()
 
-spans = wikitext.split('<span lang="')
-goiburua = spans.pop(0)
-print(f"Dokumentua kargatuta. Goiburua hau da:\n{goiburua}")
-
-result = [goiburua]
-paragraph_number = 1
-for span in spans:
-    paragraphs = f'<span lang="{span}'.split("\n\n")
-    for paragraph in paragraphs:
-        print(paragraph)
-        if not paragraph.startswith("<ins>"):
-            paragraph = "{{aingura|" + str(paragraph_number) + "}}" + paragraph
-            paragraph_number += 1
-        result.append(paragraph)
+paragraph_number = 0
+result = []
+paragraphs = wikitext.split("\n\n")
+for paragraph in paragraphs:
+    paragraph = re.sub(r'\n', '<br/>', paragraph)
+    print(paragraph)
+    if not paragraph.startswith("<span"):
+        paragraph = "{{aingura|" + str(paragraph_number) + "}}" + paragraph
+    else:
+        span_split = re.search("^(<span[^>]+>)(.*)", paragraph)
+        paragraph = span_split.group(1) + "{{aingura|" + str(paragraph_number) + "}}" + span_split.group(2)
+    paragraph_number += 1
+    result.append(paragraph)
 result_text = ("\n\n").join(result)
 
-with open('data/etxepare_ainguratuta.wikitext', 'w') as file:
+with open('data/SanAgustin_ainguratuta.wikitext', 'w') as file:
     file.write(result_text)
 
